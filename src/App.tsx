2cf4445bdfd0abda842ed5f0c79b56c4ef063023
oguz <oguzkarademir26@gmail.com>
2021-02-18 19:12:26 +0100

    fully working cart

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 
 //components
 import Item from "./item/Item";
+import Cart from "./Cart/Cart";
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
@@ -10,7 +11,7 @@ import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 
 //styles
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
 
 //types
 export type CartItemType = {
@@ -27,20 +28,58 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setcartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
-  console.log(data);
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => 
+  items.reduce((ack: number, items) => ack + items.amount, 0);
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setcartItems(prev => {
+      //1. is the item already in the cart
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
 
-  const hadleRemoveFromCart = () => null;
+      if (isItemInCart) {
+        return prev.map(item => 
+          item.id === clickedItem.id ? { ...item, amount: item.amount + 1} : item
+        );
+      }
+
+      //First time the item is added
+      return [...prev, {...clickedItem, amount: 1}];
+    })
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setcartItems(prev => (
+      prev.reduce((ack, item) => {
+        if(item.id === id){
+          if(item.amount === 1) return ack;
+          return [...ack, {...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[]) 
+    ))
+  };
 
   if(isLoading) return <LinearProgress />;
   if(error) return <div>Something went wrong ...</div>;
 
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={()=> setCartOpen(false)}>
+        <Cart 
+        cartItems={cartItems} 
+        addToCart={handleAddToCart} 
+        removeFromCart={handleRemoveFromCart}/>
+      </Drawer>
+      <StyledButton onClick={()=> setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,48 @@
+import Button from "@material-ui/core/Button";
+
+//Types
+import { CartItemType } from "../App";
+import Item from "../item/Item";
+
+//Styles
+import { Wrapper } from "./CardItem.styles";
+
+type Props = {
+    item:CartItemType;
+    addToCart:(clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+}
+
+const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => 
+    <Wrapper>
+        <div>
+            <h3>{item.title}</h3>
+            <div className="information">
+                <p>Price: ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className="buttons">
+                <Button 
+                size="small" 
+                disableElevation 
+                variant="contained" 
+                onClick={() => removeFromCart(item.id)}
+                >
+                    -
+                </Button>
+                <p>{item.amount}</p>
+                <Button 
+                size="small" 
+                disableElevation 
+                variant="contained" 
+                onClick={() => addToCart(item)}
+                >
+                    +
+                </Button>
+            </div>
+        </div>
+        <img src={item.image} alt={item.title} />
+    </Wrapper>
+;
+
+export default CartItem;
